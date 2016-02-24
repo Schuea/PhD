99b0138971a01e4731740962cc0509e45b863774
Schuea <anne.schuetz@desy.de>
2016-02-24 17:31:30 +0100

    Commented out the function again, doesn't work yet

--- code/General/src/Style_Utils.cc
+++ code/General/src/Style_Utils.cc
@@ -4,9 +4,9 @@
 #include "Style_Utils.h"
 
 void Move_StatBox(TH1* histo){
-	TPaveStats *st = (TPaveStats*)histo->GetListOfFunctions()->FindObject("stats");
-	st->SetX1NDC(0.65); //new x start position
-	st->SetX2NDC(0.85); //new x end position
-	st->SetY1NDC(0.6); //new x start position
-	st->SetY2NDC(0.9); //new x end position
+//	TPaveStats *st = (TPaveStats*)histo->GetListOfFunctions()->FindObject("stats");
+//	st->SetX1NDC(0.65); //new x start position
+//	st->SetX2NDC(0.85); //new x end position
+//	st->SetY1NDC(0.6); //new x start position
+//	st->SetY2NDC(0.9); //new x end position
 }				
