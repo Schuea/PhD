662195a129e12b8b8159c5e36427e5551330d6b4
Schuea <anne.schuetz@desy.de>
2016-04-25 11:43:04 +0200

    Plots for different layers are plotted together, statbox positions don't work properly yet

--- code/BkgSim/src/main_occupancy.cc
+++ code/BkgSim/src/main_occupancy.cc
@@ -2,6 +2,8 @@
 #include "TTree.h"
 #include "TCanvas.h"
 #include "TH1D.h"
+#include "TPaveStats.h"
+
 
 #include <bitset>
 #include <iostream>
@@ -141,22 +143,63 @@ int main(int const argc, char const * const * const argv) {
 	//Make histogram for storing the information
 	std::string const title = "Occupancy for subdetector " + subdetectornames;
 	//std::string const title = "Normalized buffer depth for subdetector " + subdetectornames;
-	TH1D *histo = new TH1D("histo", title.c_str(), 20, 0, 20);
+	std::vector< TH1D* > histos;
+	std::vector< TPaveStats* > stats;
 
+	int max_num_layers = 0;
+	for (size_t subdetector_it = 0; subdetector_it < SubDetectors->size(); ++subdetector_it) {
+		if(max_num_layers < SubDetectors->at(subdetector_it)->GetNumberOfLayers()){
+			max_num_layers = SubDetectors->at(subdetector_it)->GetNumberOfLayers();
+		}
+	}
+	std::cout<<__LINE__<<std::endl;
+	for (int number_layer = 0; number_layer <= max_num_layers; ++number_layer) {
+		std::stringstream layername;
+		layername << "Layer " << number_layer;
+		histos.emplace_back(new TH1D(layername.str().c_str(), title.c_str(), 100000, 0, 100000));
+	}
+	std::cout<<__LINE__<<std::endl;
 	for (size_t allcellhits = 0; allcellhits < AllCellHits.size(); ++allcellhits) {
 		for (size_t hitcounts = 0; hitcounts < AllCellHits.at(allcellhits)->Get_HitCount().size(); ++hitcounts) {
       if(AllCellHits.at(allcellhits)->Get_HitCount().at(hitcounts) > 0)
-			histo->Fill(AllCellHits.at(allcellhits)->Get_HitCount().at(hitcounts));
+			histos.at(AllCellHits.at(allcellhits)->Get_Layer().at(hitcounts))->Fill(AllCellHits.at(allcellhits)->Get_HitCount().at(hitcounts));
 		}
 	}
 
 	//NormalizeHistogram(histo, 1.0);
-
+	std::cout<<__LINE__<<std::endl;
 	//Plot the histogram and save it
 	TCanvas *canvas = new TCanvas("canvas", "canvas", 800, 600);
 	canvas->SetLogy(1);
-	histo->SetMinimum(0.0000001);
-	histo->Draw();
+	float boxsize = 0.0;
+	for (size_t number_histo = 0; number_histo< histos.size(); ++number_histo) {
+					histos.at(number_histo)->SetMinimum(0.1);
+					if(number_histo == 0){
+									histos.at(number_histo)->SetLineColor(2);
+									histos.at(number_histo)->Draw();
+									canvas->Update();
+									TPaveStats* st =  (TPaveStats*)histos.at(number_histo)->GetListOfFunctions()->FindObject("stats");
+									st->SetX1NDC(0.65); //new x start position
+									st->SetX2NDC(0.85); //new x end position
+									st->SetY1NDC(0.8); //new y start position
+									st->SetY2NDC(0.9); //new y end position
+									stats.push_back(st);
+									boxsize = stats.at(number_histo)->GetY2NDC() - stats.at(number_histo)->GetY1NDC();
+					}
+	std::cout<<__LINE__<<std::endl;
+					if(number_histo > 0){
+									histos.at(number_histo)->SetLineColor(2+number_histo);
+									histos.at(number_histo)->Draw("SAMES");
+									canvas->Update();
+									TPaveStats* st =  (TPaveStats*)histos.at(number_histo)->GetListOfFunctions()->FindObject("stats");
+									stats.push_back(st);
+									stats.at(number_histo)->SetTextColor(2+number_histo);
+									stats.at(number_histo)->SetX1NDC(0.65); //new x start position
+									stats.at(number_histo)->SetX2NDC(0.85); //new x end position
+									stats.at(number_histo)->SetY2NDC(stats.at(number_histo-1)->GetY1NDC()); //new y end position
+									stats.at(number_histo)->SetY1NDC(stats.at(number_histo-1)->GetY2NDC()-boxsize); //new y start position
+					}
+	}
   std::stringstream output;
   output << "output/occupancy_" << subdetectornames;
 	canvas->Print((output.str() + ".pdf").c_str());
