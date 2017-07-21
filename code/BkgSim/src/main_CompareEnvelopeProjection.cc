03ff1e6687dd150aaf15a141dcba39db2e4c41e6
Anne Schuetz <anne.schuetz@desy.de>
2017-07-21 17:09:58 +0200

    Finished: Added legend, UsePhDStyle, Lines indicating the beampipe; set the graph markers

--- code/BkgSim/src/main_CompareEnvelopeProjection.cc
+++ code/BkgSim/src/main_CompareEnvelopeProjection.cc
@@ -1,14 +1,18 @@
 #include "TFile.h"
 #include "TH1.h"
 #include "TCanvas.h"
+#include "TFrame.h"
 #include "TStyle.h"
 #include "TLegend.h"
+#include "TLine.h"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "Style.h"
 
 int main(int const argc, char const * const * const argv){
+  UsePhDStyle();
 
 	std::vector< std::string > inputfilenames;
 	std::string outputfilename;
@@ -54,9 +58,10 @@ int main(int const argc, char const * const * const argv){
   canvas->SetGridy();
   gStyle->SetOptStat(0);
 
-  TLegend* legend = new TLegend(0.13,0.7,0.55,0.88);
+  TLegend* legend = new TLegend(0.58,0.65,0.98,0.85);
+  legend->SetFillColor(kWhite);
   std::string name;
-
+  
 	for(size_t no_files = 0; no_files < inputfilenames.size(); ++ no_files){
     name = " ";
 
@@ -65,9 +70,12 @@ int main(int const argc, char const * const * const argv){
     if( (bool)ifile == 1){
       TH1F* temp = (TH1F*) inputfile->Get("Projection_xz_1Kink");
       temp->SetLineWidth(2);
+      temp->SetMarkerSize(0.7);
       if(no_files == 0){
         name = "set  2: TDR";
         temp->SetLineColor(1);
+        temp->SetMarkerColor(1);
+        temp->SetMarkerStyle(4);
         temp->GetXaxis()->SetTitle("x [mm]");
         temp->GetYaxis()->SetTitle("Number of pair background particles");
         temp->Draw();
@@ -75,19 +83,25 @@ int main(int const argc, char const * const * const argv){
       else{
         if(no_files == 1){
           temp->SetLineColor(2);
+          temp->SetMarkerColor(2);
+          temp->SetMarkerStyle(8);
           name = "set  4: TDR + Emittance_x";
         }
         if(no_files == 2){
           temp->SetLineColor(3);
+          temp->SetMarkerColor(3);
+          temp->SetMarkerStyle(25);
           name = "set 15: TDR + Emittance_x + Beta_x";
         }
         if(no_files == 3){
           temp->SetLineColor(4);
+          temp->SetMarkerColor(4);
+          temp->SetMarkerStyle(21);
           name = "set 16: TDR + Emittance_x + Beta_x + Beta_y";
         }
         temp->Draw("SAMES");
       }
-      legend->AddEntry(temp,name.c_str(),"l");
+      legend->AddEntry(temp,name.c_str(),"p");
       //inputfile->Close();
 		}
 		else{
@@ -96,8 +110,25 @@ int main(int const argc, char const * const * const argv){
 		}
 
 	}
-  legend->Draw();
+  canvas->Update();
 
+  float ymin = -0.001; 
+  float ymax = 4.0*std::pow(10.0,4.0);
+  //ymin = gPad->GetFrame()->GetY1(); 
+  //ymax = gPad->GetFrame()->GetY2();
+  //std::cout << "ymin, ymax: " << ymin << ", " << ymax << std::endl;
+
+  TLine* line = new TLine(-12,ymin,-12,ymax);
+  TLine* line2 = new TLine(12,ymin,12,ymax);
+  line->SetLineColor(kMagenta);
+  line->SetLineWidth(2);
+  line2->SetLineColor(kMagenta);
+  line2->SetLineWidth(2);
+  line->Draw();
+  line2->Draw();
+
+  legend->Draw();
+  
   canvas->Print( ("output/HelixEnvelope_Projection_Comparison_"+outputfilename+".pdf").c_str() );
   canvas->Print( ("output/HelixEnvelope_Projection_Comparison_"+outputfilename+".cxx").c_str() );
 
