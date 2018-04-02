a7d14ddaaa9d76586c51e87d9d216094c28a9fc2
Schuea <anne.schuetz@desy.de>
2018-04-02 16:31:52 +0200

    Optimized mgdraw routine from Francesco Cerutti, running a long run with this routine

---
+++ code/BeamDump/BeamDump_Design_BSmith_0_TB_0067_210_00_A/Neutron_spectrum/mgdraw.f
@@ -0,0 +1,192 @@
+*$ CREATE MGDRAW.FOR
+*COPY MGDRAW
+*                                                                      *
+*=== mgdraw ===========================================================*
+*                                                                      *
+      SUBROUTINE MGDRAW ( ICODE, MREG )
+
+      INCLUDE '(DBLPRC)'
+      INCLUDE '(DIMPAR)'
+      INCLUDE '(IOUNIT)'
+*
+*----------------------------------------------------------------------*
+*                                                                      *
+*     Copyright (C) 1990-2013      by        Alfredo Ferrari           *
+*     All Rights Reserved.                                             *
+*                                                                      *
+*                                                                      *
+*     MaGnetic field trajectory DRAWing: actually this entry manages   *
+*                                        all trajectory dumping for    *
+*                                        drawing                       *
+*                                                                      *
+*     Created on   01 March 1990   by        Alfredo Ferrari           *
+*                                              INFN - Milan            *
+*     Last change   12-Nov-13      by        Alfredo Ferrari           *
+*                                              INFN - Milan            *
+*                                                                      *
+*----------------------------------------------------------------------*
+*
+      INCLUDE '(CASLIM)'
+      INCLUDE '(COMPUT)'
+      INCLUDE '(SOURCM)'
+      INCLUDE '(FHEAVY)'
+      INCLUDE '(FLKSTK)'
+      INCLUDE '(GENSTK)'
+      INCLUDE '(MGDDCM)'
+      INCLUDE '(PAPROP)'
+      INCLUDE '(QUEMGD)'
+      INCLUDE '(SUMCOU)'
+      INCLUDE '(TRACKR)'
+*
+      DIMENSION DTQUEN ( MXTRCK, MAXQMG )
+*
+      CHARACTER*20 FILNAM
+      LOGICAL LFCOPE, LFIRST
+      SAVE LFCOPE
+      DATA LFCOPE, LFIRST / .FALSE., .TRUE. /
+*
+      SAVE NRFROM,NRTOWA
+*     
+*----------------------------------------------------------------------*
+*                                                                      *
+*     Icode = 1: call from Kaskad                                      *
+*     Icode = 2: call from Emfsco                                      *
+*     Icode = 3: call from Kasneu                                      *
+*     Icode = 4: call from Kashea                                      *
+*     Icode = 5: call from Kasoph                                      *
+*                                                                      *
+*----------------------------------------------------------------------*
+*                                                                      *
+      RETURN
+*
+*======================================================================*
+*                                                                      *
+*     Boundary-(X)crossing DRAWing:                                    *
+*                                                                      *
+*     Icode = 1x: call from Kaskad                                     *
+*             19: boundary crossing                                    *
+*     Icode = 2x: call from Emfsco                                     *
+*             29: boundary crossing                                    *
+*     Icode = 3x: call from Kasneu                                     *
+*             39: boundary crossing                                    *
+*     Icode = 4x: call from Kashea                                     *
+*             49: boundary crossing                                    *
+*     Icode = 5x: call from Kasoph                                     *
+*             59: boundary crossing                                    *
+*                                                                      *
+*======================================================================*
+*                                                                      *
+      ENTRY BXDRAW ( ICODE, MREG, NEWREG, XSCO, YSCO, ZSCO )
+      IF ( .NOT. LFCOPE ) THEN
+        LFCOPE = .TRUE.
+        OPEN ( UNIT = 93, FILE = "Neut_OUT", STATUS = 'UNKNOWN', FORM =
+     *     'FORMATTED')
+      END IF 
+      IF(LFIRST) THEN
+         CALL GEON2R ('ScorePl1',NRFROM,IERR1 )
+         CALL GEON2R ('EXTAir  ',NRTOWA,IERR2 )
+         IF(IERR1.NE.0.OR.IERR2.NE.0) STOP "Error in name conversion"
+         LFIRST = .FALSE.
+      END IF
+      IF(MREG.EQ.NRFROM.AND.NEWREG.EQ.NRTOWA) THEN
+         IF(JTRACK.EQ.8) THEN ! select neutron
+            WRITE(93,*) XSCO, YSCO, ZSCO, CXTRCK, CYTRCK, CZTRCK, 
+     *         ETRACK-AM(JTRACK), ATRACK, WTRACK, JTRACK, KTRACK
+         ENDIF
+      ENDIF 
+      RETURN
+*
+*======================================================================*
+*                                                                      *
+*     Event End DRAWing:                                               *
+*                                                                      *
+*======================================================================*
+*                                                                      *
+      ENTRY EEDRAW ( ICODE )
+      RETURN
+*
+*======================================================================*
+*                                                                      *
+*     ENergy deposition DRAWing:                                       *
+*                                                                      *
+*     Icode = 1x: call from Kaskad                                     *
+*             10: elastic interaction recoil                           *
+*             11: inelastic interaction recoil                         *
+*             12: stopping particle                                    *
+*             13: pseudo-neutron deposition                            *
+*             14: escape                                               *
+*             15: time kill                                            *
+*     Icode = 2x: call from Emfsco                                     *
+*             20: local energy deposition (i.e. photoelectric)         *
+*             21: below threshold, iarg=1                              *
+*             22: below threshold, iarg=2                              *
+*             23: escape                                               *
+*             24: time kill                                            *
+*     Icode = 3x: call from Kasneu                                     *
+*             30: target recoil                                        *
+*             31: below threshold                                      *
+*             32: escape                                               *
+*             33: time kill                                            *
+*     Icode = 4x: call from Kashea                                     *
+*             40: escape                                               *
+*             41: time kill                                            *
+*             42: delta ray stack overflow                             *
+*     Icode = 5x: call from Kasoph                                     *
+*             50: optical photon absorption                            *
+*             51: escape                                               *
+*             52: time kill                                            *
+*                                                                      *
+*======================================================================*
+*                                                                      *
+      ENTRY ENDRAW ( ICODE, MREG, RULL, XSCO, YSCO, ZSCO )
+      RETURN
+*
+*======================================================================*
+*                                                                      *
+*     SOurce particle DRAWing:                                         *
+*                                                                      *
+*======================================================================*
+*
+      ENTRY SODRAW
+      RETURN
+*
+*======================================================================*
+*                                                                      *
+*     USer dependent DRAWing:                                          *
+*                                                                      *
+*     Icode = 10x: call from Kaskad                                    *
+*             100: elastic   interaction secondaries                   *
+*             101: inelastic interaction secondaries                   *
+*             102: particle decay  secondaries                         *
+*             103: delta ray  generation secondaries                   *
+*             104: pair production secondaries                         *
+*             105: bremsstrahlung  secondaries                         *
+*             110: decay products                                      *
+*     Icode = 20x: call from Emfsco                                    *
+*             208: bremsstrahlung secondaries                          *
+*             210: Moller secondaries                                  *
+*             212: Bhabha secondaries                                  *
+*             214: in-flight annihilation secondaries                  *
+*             215: annihilation at rest   secondaries                  *
+*             217: pair production        secondaries                  *
+*             219: Compton scattering     secondaries                  *
+*             221: photoelectric          secondaries                  *
+*             225: Rayleigh scattering    secondaries                  *
+*             237: mu pair     production secondaries                  *
+*     Icode = 30x: call from Kasneu                                    *
+*             300: interaction secondaries                             *
+*     Icode = 40x: call from Kashea                                    *
+*             400: delta ray  generation secondaries                   *
+*  For all interactions secondaries are put on GENSTK common (kp=1,np) *
+*  but for KASHEA delta ray generation where only the secondary elec-  *
+*  tron is present and stacked on FLKSTK common for kp=npflka          *
+*                                                                      *
+*======================================================================*
+*
+      ENTRY USDRAW ( ICODE, MREG, XSCO, YSCO, ZSCO )
+* No output by default:
+      RETURN
+
+*=== End of subrutine Mgdraw ==========================================*
+      END
+
